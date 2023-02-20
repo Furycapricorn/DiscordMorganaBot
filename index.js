a067a23f6d26edcadf5dec57c38a4fdb9392d6a6
Furycapricorn <maxi.hartmann98@hotmail.de>
2023-02-20 20:04:27 +0100

    changed to switch case

--- index.js
+++ index.js
@@ -29,73 +29,84 @@ const client = new Client({
 client.on("ready", async () => {
   console.log(`logged in as ${client.user.tag}`);
 
-  console.log('Send Button')
-  const buttonRow = new ActionRowBuilder()
-    .addComponents(
-      new ButtonBuilder()
-        .setCustomId('acceptButton')
-        .setLabel('Accept')
-        .setStyle(ButtonStyle.Primary),
-      new ButtonBuilder()
-        .setCustomId('declineButton')
-        .setLabel('Decline')
-        .setStyle(ButtonStyle.Secondary)
-    );
-    //message.reply({ content: 'Do you accept your fate', components: [row] });
-    
-  const ruleChannel = client.channels.cache.get(`1077287262754197625`)
-  const fetched = await ruleChannel.messages.fetch({limit: 2})
-  console.log(fetched)
-  ruleChannel.bulkDelete(fetched)
-  ruleChannel.send({ content: 'Do you accept your fate', components: [buttonRow] })
-  
 });
 
 //Listen to new messages on the server
 client.on("messageCreate", async (message) => {
   console.log('recived message: '+ message.content)
   
-  if (message.content === "ping") {
-    message.reply("pong");
-    }
-    if (message.content === "try embed"){
+  //skipps botmessages
+  if (message.author.bot){
+    console.log("Bot message catched")
+    return;
+  } 
+  switch(message.content){
+    case "ping":
+      message.reply("pong");
+      break
+    case "try embed":
       console.log(`Embed Message send`);
       const exampleEmbed = new EmbedBuilder()
       .setColor(0xA02323)
-      .setTitle('You have been selected to become part of the Phantom Thieves! Here are some rules before we can start stealing hearts together.')
-      .setURL('https://discord.js.org/')
-      .setAuthor({ name: 'Joker', iconURL: 'https://cdna.artstation.com/p/assets/images/images/014/300/896/large/connor-mcleod-lookincooljoker.jpg', url: 'https://discord.js.org' })
-      .setDescription('Sehr wichtige Regeln. Also wirklich super wichtig. Du hast keine Vorstellung wie unfassbar wichtig diese Regeln sind die hier alle stehen könnten. Hoo boy, das ist wirklich sehr wichtig und informativ hier. Du bist bestimmt genauso froh wie ich, dass du so weit gelesen hast. Also Belohnung hast du dir einen Cookie verdient. :cookie:')
+      .setTitle('Come forth my other self!')
+      //.setURL('https://discord.js.org/')
+      .setAuthor({ name: 'Joker', iconURL: 'http://pm1.narvii.com/7378/334890432d46b6dc56b7ae6d8766c78f57e01853r1-443-443v2_00.jpg' })
+      .setDescription('You have been selected to become part of the Phantom Thieves! Here are some rules before we can start stealing hearts together.')
       //.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-      /*.addFields(
-        { name: 'Regular field title', value: 'Some value here' },
-        { name: '\u200B', value: '\u200B' },
+      .addFields(
+        { name: 'Regel Nummer 1', value: 'Sehr wichtige Regeln. Also wirklich super wichtig. Du hast keine Vorstellung wie unfassbar wichtig diese Regeln sind die hier alle stehen könnten. '},
+        { name: 'Regel Nummer 2', value: 'Hoo boy, das ist wirklich sehr wichtig und informativ hier. Du bist bestimmt genauso froh wie ich, dass du so weit gelesen hast. Also Belohnung hast du dir einen Cookie verdient. :cookie:' },
+        /*{ name: '\u200B', value: '\u200B' },
         { name: 'Inline field title', value: 'Some value here', inline: true },
-        { name: 'Inline field title', value: 'Some value here', inline: true },
+        { name: 'Inline field title', value: 'Some value here', inline: true },*/
       )
-      .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+      /*.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
       .setImage('https://i.imgur.com/AfFp7pu.png')*/
       .setTimestamp()
-      .setFooter({ text: 'Bitte bestätige unten, dass du verstanden hast.', iconURL: 'https://cdna.artstation.com/p/assets/images/images/014/300/896/large/connor-mcleod-lookincooljoker.jpg' });
+      .setFooter({ text: 'Bitte bestätige unten, dass du verstanden hast.', iconURL: 'http://pm1.narvii.com/7378/334890432d46b6dc56b7ae6d8766c78f57e01853r1-443-443v2_00.jpg' });
       console.log({ embeds: [exampleEmbed] })
-  
       message.channel.send({ embeds: [exampleEmbed] });
-    }
-  if (message.content === "button"){
-    console.log('Send Button')
-    const row = new ActionRowBuilder()
-			.addComponents(
-				new ButtonBuilder()
-					.setCustomId('acceptButton')
-					.setLabel('Accept')
-					.setStyle(ButtonStyle.Primary),
-        new ButtonBuilder()
-          .setCustomId('declineButton')
-          .setLabel('Decline')
-          .setStyle(ButtonStyle.Secondary)
-			);
-    await message.reply({ content: 'Do you accept your fate', components: [row] });
+      break
+    case "button":
+      console.log('Send Button')
+      const row = new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+            .setCustomId('acceptButton')
+            .setLabel('Accept')
+            .setStyle(ButtonStyle.Primary),
+          new ButtonBuilder()
+            .setCustomId('declineButton')
+            .setLabel('Decline')
+            .setStyle(ButtonStyle.Secondary)
+        );
+      await message.reply({ content: 'Do you accept your fate', components: [row] });
+      break
+    case "new rules":
+      if (message.channelId ==='1077287262754197625') {
+        console.log('Rule Button')
+      const buttonRow = new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+            .setCustomId('acceptRules')
+            .setLabel('Accept')
+            .setStyle(ButtonStyle.Primary),
+          new ButtonBuilder()
+            .setCustomId('declineRules')
+            .setLabel('Decline')
+            .setStyle(ButtonStyle.Secondary)
+        );
+        const ruleChannel = client.channels.cache.get(`1077287262754197625`)
+        const fetched = await ruleChannel.messages.fetch({limit: 20})
+        ruleChannel.bulkDelete(fetched)
+        ruleChannel.send({ content: 'Do you accept your fate', components: [buttonRow] })
+        console.log("new rules")
+      } else {
+        await message.reply({ content: 'Only allowed in rules', ephemeral: true })
+      }
+      break  
   }
+
 });
 
 client.on(Events.MessageReactionAdd, async (reaction, user) => {
