aef58d8b83abd1affa967ec31a9f26c9da7391d5
Furycapricorn <maxi.hartmann98@hotmail.de>
2023-02-20 19:17:47 +0100

    Deletes old rules now

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new Client({
 });
 
 //Listen to the event that signals the bot is ready to start working
-client.on("ready", () => {
+client.on("ready", async () => {
   console.log(`logged in as ${client.user.tag}`);
 
   console.log('Send Button')
@@ -42,10 +42,13 @@ client.on("ready", () => {
         .setStyle(ButtonStyle.Secondary)
     );
     //message.reply({ content: 'Do you accept your fate', components: [row] });
+    
   const ruleChannel = client.channels.cache.get(`1077287262754197625`)
-  const fetched = ruleChannel.fetchMessages({limit: 2})
+  const fetched = await ruleChannel.messages.fetch({limit: 2})
+  console.log(fetched)
   ruleChannel.bulkDelete(fetched)
   ruleChannel.send({ content: 'Do you accept your fate', components: [buttonRow] })
+  
 });
 
 //Listen to new messages on the server
@@ -55,29 +58,29 @@ client.on("messageCreate", async (message) => {
   if (message.content === "ping") {
     message.reply("pong");
     }
-  if (message.content === "try embed"){
-    console.log(`Embed Message send`);
-    const exampleEmbed = new EmbedBuilder()
-    .setColor(0x0099FF)
-    .setTitle('Some title')
-    .setURL('https://discord.js.org/')
-    .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-    .setDescription('Some description here')
-    .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-    .addFields(
-      { name: 'Regular field title', value: 'Some value here' },
-      { name: '\u200B', value: '\u200B' },
-      { name: 'Inline field title', value: 'Some value here', inline: true },
-      { name: 'Inline field title', value: 'Some value here', inline: true },
-    )
-    .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-    .setImage('https://i.imgur.com/AfFp7pu.png')
-    .setTimestamp()
-    .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-    console.log({ embeds: [exampleEmbed] })
-
-    message.channel.send({ embeds: [exampleEmbed] });
-  }
+    if (message.content === "try embed"){
+      console.log(`Embed Message send`);
+      const exampleEmbed = new EmbedBuilder()
+      .setColor(0xA02323)
+      .setTitle('You have been selected to become part of the Phantom Thieves! Here are some rules before we can start stealing hearts together.')
+      .setURL('https://discord.js.org/')
+      .setAuthor({ name: 'Joker', iconURL: 'https://cdna.artstation.com/p/assets/images/images/014/300/896/large/connor-mcleod-lookincooljoker.jpg', url: 'https://discord.js.org' })
+      .setDescription('Sehr wichtige Regeln. Also wirklich super wichtig. Du hast keine Vorstellung wie unfassbar wichtig diese Regeln sind die hier alle stehen könnten. Hoo boy, das ist wirklich sehr wichtig und informativ hier. Du bist bestimmt genauso froh wie ich, dass du so weit gelesen hast. Also Belohnung hast du dir einen Cookie verdient. :cookie:')
+      //.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+      /*.addFields(
+        { name: 'Regular field title', value: 'Some value here' },
+        { name: '\u200B', value: '\u200B' },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+      )
+      .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+      .setImage('https://i.imgur.com/AfFp7pu.png')*/
+      .setTimestamp()
+      .setFooter({ text: 'Bitte bestätige unten, dass du verstanden hast.', iconURL: 'https://cdna.artstation.com/p/assets/images/images/014/300/896/large/connor-mcleod-lookincooljoker.jpg' });
+      console.log({ embeds: [exampleEmbed] })
+  
+      message.channel.send({ embeds: [exampleEmbed] });
+    }
   if (message.content === "button"){
     console.log('Send Button')
     const row = new ActionRowBuilder()
