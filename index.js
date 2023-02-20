b5697a7d5de5af11d5af38ed9cf957f8df98ce9d
Furycapricorn <maxi.hartmann98@hotmail.de>
2023-02-20 16:17:14 +0100

    notice a reaction on old messages

--- index.js
+++ index.js
@@ -1,19 +1,66 @@
 //index.js
+const { Client, Events, GatewayIntentBits, Partials, MessageEmbed} = require('discord.js');
+require('dotenv').config()
 
-require("dotenv").config();
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
+	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+});
 
 //Listen to the event that signals the bot is ready to start working
 client.on("ready", () => {
   console.log(`logged in as ${client.user.tag}`);
+  client.channels.cache.get(`1076914336745922725`).send(`Moin Joker o/`)
 });
 
 //Listen to new messages on the server
 client.on("message", async (message) => {
+  console.log('recived message')
   if (message.content === "ping") {
     message.reply("pong");
+    }
+  if (message.content === "try embed"){
+    console.log(`logged in as ${client.user.tag}`);
+    const exampleEmbed = new MessageEmbed()
+    .setColor(0x0099FF)
+    .setTitle('Some title')
+    .setURL('https://discord.js.org/')
+    .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+    .setDescription('Some description here')
+    .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+    .addFields(
+      { name: 'Regular field title', value: 'Some value here' },
+      { name: '\u200B', value: '\u200B' },
+      { name: 'Inline field title', value: 'Some value here', inline: true },
+      { name: 'Inline field title', value: 'Some value here', inline: true },
+    )
+    .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+    .setImage('https://i.imgur.com/AfFp7pu.png')
+    .setTimestamp()
+    .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+    console.log({ embeds: [exampleEmbed] })
+
+    message.channel.send({ embeds: [exampleEmbed] });
   }
+});
+
+client.on(Events.MessageReactionAdd, async (reaction, user) => {
+	// When a reaction is received, check if the structure is partial
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message:', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+	}
+
+	// Now the message has been cached and is fully available
+	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+	// The reaction is now also fully available and the properties will be reflected accurately:
+	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
 });
 
 //Login to the server using bot token
